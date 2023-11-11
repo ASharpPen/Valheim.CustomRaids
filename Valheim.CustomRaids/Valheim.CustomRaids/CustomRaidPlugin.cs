129a549957dcd23a8011c3dc8b0a775b05cc7b97
Asharppen <asharppen@gmail.com>
2023-11-11 15:17:29 +0100

    Documentation and version bump

--- Valheim.CustomRaids/Valheim.CustomRaids/CustomRaidPlugin.cs
+++ Valheim.CustomRaids/Valheim.CustomRaids/CustomRaidPlugin.cs
@@ -15,7 +15,7 @@ public class CustomRaidPlugin : BaseUnityPlugin
 {
     public const string ModId = "asharppen.valheim.custom_raids";
     public const string PluginName = "Custom Raids";
-    public const string Version = "1.7.9";
+    public const string Version = "1.7.10";
 
     void Awake()
     {
