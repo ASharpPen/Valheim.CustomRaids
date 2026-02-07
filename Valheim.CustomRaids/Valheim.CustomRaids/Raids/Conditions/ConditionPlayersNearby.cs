8ad3dda4e68d93215cf72ba581875fae9a722387
ASharpPen <>
2026-02-07 14:58:20 +0100

    Fixed player counting in ConditionPlayersNearby

--- Valheim.CustomRaids/Valheim.CustomRaids/Raids/Conditions/ConditionPlayersNearby.cs
+++ Valheim.CustomRaids/Valheim.CustomRaids/Raids/Conditions/ConditionPlayersNearby.cs
@@ -1,4 +1,4 @@
-﻿using Valheim.CustomRaids.Utilities;
+﻿using Valheim.CustomRaids.Core;
 
 namespace Valheim.CustomRaids.Raids.Conditions
 {
@@ -11,10 +11,26 @@ namespace Valheim.CustomRaids.Raids.Conditions
         public float Distance { get; set; } = 200;
 
         public bool IsValid(RaidContext context)
-        {
-            var playersNearby = PlayerUtils
-                .GetPlayersInRadius(context.Position, Distance)
-                .Count;
+        {            
+            // If not a multiplayer game, skip this condition.
+            if (ZNet.m_isServer &&
+                !ZNet.m_openServer)
+            {
+                Log.LogDebug($"[{nameof(ConditionPlayersNearby)}] Not a multiplayer game. Skipping condition.");
+                return true;
+            }
+
+            var connections = ZNet.instance.GetConnectedPeers();
+
+            int playersNearby = 0;
+
+            foreach (var connection in connections)
+            {
+                if (Utils.DistanceXZ(connection.GetRefPos(), context.Position) <= Distance)
+                {
+                    playersNearby++;
+                }
+            }
 
             if (MinPlayers is not null)
             {
